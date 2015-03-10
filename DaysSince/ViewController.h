34fb4229ccc7560d5f751a1d89bc8b9dfa427e91
Sam Cole <scole3@uic.edu>
2015-03-09 20:49:02 -0500

    Initial Commit

---
+++ DaysSince/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaysSince
+//
+//  Created by Sam Cole on 3/9/15.
+//  Copyright (c) 2015 blah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DaysSince/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DaysSince
+//
+//  Created by Sam Cole on 3/9/15.
+//  Copyright (c) 2015 blah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

96920dc98dbaac8176d178233c89a90f69647e79
Sam Cole <scole3@uic.edu>
2015-03-14 00:10:00 -0500

    Create DaysSinceShared framework to hold resources shared by app and widget

---
+++ DaysSinceShared/DaysSinceShared.h
@@ -0,0 +1,17 @@
+//
+//  DaysSinceShared.h
+//  DaysSinceShared
+//
+//  Created by Sam Cole on 3/14/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DaysSinceShared.
+FOUNDATION_EXPORT double DaysSinceSharedVersionNumber;
+
+//! Project version string for DaysSinceShared.
+FOUNDATION_EXPORT const unsigned char DaysSinceSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaysSinceShared/PublicHeader.h>
---
+++ DaysSinceShared/DaysSinceShared.m
@@ -0,0 +1,10 @@
+//
+//  DaysSinceShared.m
+//  DaysSince
+//
+//  Created by Sam Cole on 3/14/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "DaysSinceShared.h"

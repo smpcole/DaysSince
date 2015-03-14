d5a5f5661fd69d9a6689171f64c1d75653d6d718
Sam Cole <scole3@uic.edu>
2015-03-14 00:50:36 -0500

    Add DaysSinceWidget target for today extension
    See #2

---
+++ DaysSinceWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  DaysSinceWidget
+//
+//  Created by Sam Cole on 3/14/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end

036e96aa9425abbb1817dee15db42bc5377bcf30
Sam Cole <scole3@uic.edu>
2015-03-16 20:06:13 -0500

    Add RootViewController class

---
+++ DaysSince/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  DaysSince
+//
+//  Created by Sam Cole on 3/16/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@end

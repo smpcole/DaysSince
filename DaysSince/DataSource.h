49e0d64a698c22f8c599dcf768ececdc108fa555
Sam Cole <scole3@uic.edu>
2015-03-16 21:00:18 -0500

    Add DataSource class
    Class will serve as the data source for UIPageViewController and handle switching between pages.

---
+++ DaysSince/DataSource.h
@@ -0,0 +1,15 @@
+//
+//  DataSource.h
+//  DaysSince
+//
+//  Created by Sam Cole on 3/16/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface DataSource : NSObject <UIPageViewControllerDataSource>
+
+@end
---
+++ DaysSince/DataSource.m
@@ -0,0 +1,13 @@
+//
+//  DataSource.m
+//  DaysSince
+//
+//  Created by Sam Cole on 3/16/15.
+//
+//
+
+#import "DataSource.h"
+
+@implementation DataSource
+
+@end

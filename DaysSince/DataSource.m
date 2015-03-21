b9fcdf6275f552fe9307de776a43a886e317cfdd
Sam Cole <scole3@uic.edu>
2015-03-20 20:11:32 -0500

    Add init method to DataSource
    For now, it just initializes numViews to the number of counters stored on disk.

--- DaysSince/DataSource.m
+++ DaysSince/DataSource.m
@@ -7,7 +7,17 @@
 //
 
 #import "DataSource.h"
+#import "DaysSinceShared.h"
 
 @implementation DataSource
+
+- (id)init {
+    self = [super init];
+    if(self) {
+        self.numViews = numStoredCounters();
+        NSLog(@"%d counter(s) found on disk", self.numViews);
+    }
+    return self;
+}
 
 @end
